9ae4700e105d95dc9b4a9e2eb4a0a96ce01a889b
Sup3r-Us3r <maydersonmello@gmail.com>
2020-05-15 09:46:04 -0300

    changed baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
+});
 
 export default api;
