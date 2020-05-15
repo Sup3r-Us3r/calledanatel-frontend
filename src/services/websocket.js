c5c69973a1498a1ebe942635a20dc7771f75faa1
Sup3r-Us3r <maydersonmello@gmail.com>
2020-05-15 09:46:41 -0300

    changed socketio url

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,6 +1,8 @@
 import socketio from 'socket.io-client';
 
-const socket = socketio(process.env.REACT_APP_API_URL);
+const socket = socketio(
+  process.env.REACT_APP_API_URL || 'http://localhost:3333'
+);
 
 socket.on('connect', () =>
   console.log('[IO] Connect => A new connection has been established')
